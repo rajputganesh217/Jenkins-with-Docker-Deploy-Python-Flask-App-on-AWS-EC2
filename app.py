67979eae40b45f3bc0c17d786ab741917b16bca6
rajputganesh217 <159476491+rajputganesh217@users.noreply.github.com>
2025-08-04 01:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "🚀 Flask-App Deployed using Jenkins CI/CD + Docker!"
+    return "🚀 Flask App Deployed using Jenkins CI/CD + Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
